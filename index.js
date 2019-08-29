5b3f41994c10dba763c5df57753227e08392fcc8
view <akimuraray@gmail.com>
2019-08-29 19:20:20 +0700

    make it better

--- index.js
+++ index.js
@@ -10,8 +10,23 @@ const PORT = process.env.PORT || 9999;
 const request = require('request');
 const bodyParser = require('body-parser');
 
-server()
+
+
+    server()
     .use(bodyParser.json())
     .use(bodyParser.urlencoded({ extended: false}))
     .get('/', (req, res) => res.send(`Irunaonline Chat bot nodejs-line-api running on PORT: ${ PORT }`))
+    // เพิ่มส่วนของ Webhook เข้าไป
+    .post('/webhook', function (req, res) {
+        let replyToken = req.body.events[0].replyToken;
+        let msg = req.body.events[0].message.text;
+        
+        console.log(`Message token : ${ replyToken }`);
+        console.log(`Message from chat : ${ msg }`);
+
+        res.json({
+            status: 200,
+            message: 'Webhook is working!'
+        });
+    })
     .listen(PORT, () => console.log(`iruna chat bot on ${ PORT }`));
